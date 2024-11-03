45586c765e6cfc82eeb6b90b55aaa15eb7344ae0
psegura- <pepeseguracarreno@gmail.com>
2024-11-03 22:30:50 +0100

    Added log file backup logic

--- inc/reporter/Tintin_reporter.hpp
+++ inc/reporter/Tintin_reporter.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
-# include <iostream>
+
+# include <sys/stat.h>  // For mkdir
+# include <fcntl.h>     // For open
+# include <unistd.h>    // For close
+# include <errno.h>     // For errno and strerror
+# include <cstring>     // For strerror
+# include <iostream>    // For std::cerr
+# include <string>      // For std::string
+# include <ctime>       // For std::time and std::localtime
+
 # include <fstream>
-# include <ctime>
 # include <iomanip>
-# include <unistd.h>
-# include <fcntl.h>
-# include <sys/stat.h>
 
 # define RESET  "\033[0m"
 # define GREY   "\033[38;5;240m"
@@ -15,6 +20,9 @@
 # define ORANGE "\033[38;5;214m"
 # define RED    "\033[31m"
 
+# define MAX_MSGS 4
+
+constexpr size_t MAX_FILE_SIZE = 10 * 1024;
 
 class Tintin_reporter
 {
@@ -28,9 +36,9 @@ class Tintin_reporter
         void    error(const std::string log);
     
     private:
-        std::ostream*   _output;
-        std::ofstream   _file;
         std::string     _filename;
+        std::string     _backup_dir;
+
         std::string     _log_msg;
         int             _fd;
         bool            _pretty_format;
@@ -39,6 +47,7 @@ class Tintin_reporter
         void print_time(void);
         void print_mode(const std::string mode);
         void open_file(void);
+        void backup_file(void);
         void print_buffer(void);
 
         Tintin_reporter();
